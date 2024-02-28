57a5051aa3db496f2e6cd4135b6b154f72027acc
Arifuwu <103443173+ArmanZ94@users.noreply.github.com>
2024-02-28 22:29:42 +0700

    Update sdle.lua
    - Bug Fix; wraparound the screen bug effect
    - Removing "Lensflare mode" because too much issue + it's not that good if the map doesn't fit well

--- lua/postprocess/sdle.lua
+++ lua/postprocess/sdle.lua
@@ -1,13 +1,11 @@
 if CLIENT then
-	local dl_intensity = CreateClientConVar("pp_dlens_intensity", "12", true, false)
-	local dl_blur = CreateClientConVar("pp_dlens_blurrange", "28", true, false)
-	local dl_lenson = CreateClientConVar("pp_dlens", "1", true, false)
-	local dl_mul = CreateClientConVar("pp_dlens_mul", "1", true, false)
-	local dl_darken = CreateClientConVar("pp_dlens_darken", "0.45", true, false)
-	local dl_mode = CreateClientConVar("pp_dlens_mode", "1", true, false)
+	local dl_enable = CreateClientConVar("pp_dlens", "0", true, false)
+	local dl_blurrange = CreateClientConVar("pp_dlens_blurrange", "10", true, false)
+	local dl_blurpasses = CreateClientConVar("pp_dlens_blurpasses", "30", true, false)
+	local dl_multiply = CreateClientConVar("pp_dlens_multiply", "1", true, false)
+	local dl_darken = CreateClientConVar("pp_dlens_darken", "0.30", true, false)
 	local dl_texture = CreateClientConVar("pp_dlens_texture", "dlenstexture/dlensdefault", true, false)
-	local dl_fov = GetConVar("fov_desired")
-	
+
 	list.Set("PostProcess", "Dirt Lens", {
 		icon = "gui/postprocess/sdle.jpg",
 		convar = "pp_dlens",
@@ -22,10 +20,9 @@ if CLIENT then
 			}
 
 			params.Options["#preset.default"] = {
-				pp_dlens_intensity = "30",
-				pp_dlens_blurrange = "10",
-				pp_dlens_mul = "1",
-				pp_dlens_mode = "1",
+				pp_dlens_blurrange = "15",
+				pp_dlens_blurpasses = "30",
+				pp_dlens_multiply = "1.30",
 				pp_dlens_darken = "0.30",
 				pp_dlens_texture = "dlenstexture/dlensdefault"
 			}				
@@ -66,12 +63,12 @@ if CLIENT then
 				Command = "pp_dlens_blurrange",
 				Type = "Integer",
 				Min = "0",
-				Max = "100"
+				Max = "50"
 			})
 
 			CPanel:AddControl("Slider", {
 				Label = "Passes",
-				Command = "pp_dlens_intensity",
+				Command = "pp_dlens_blurpasses",
 				Type = "Integer",
 				Min = "0",
 				Max = "30"
@@ -79,7 +76,7 @@ if CLIENT then
 
 			CPanel:AddControl("Slider", {
 				Label = "Multiply",
-				Command = "pp_dlens_mul",
+				Command = "pp_dlens_multiply",
 				Type = "Float",
 				Min = "0",
 				Max = "10"
@@ -92,100 +89,48 @@ if CLIENT then
 				Min = "0",
 				Max = "1"
 			})
-			
-			CPanel:ControlHelp(" ")
-			CPanel:AddControl("Label", {Text = ":EXPERIMENTAL:"})
-			CPanel:AddControl("Label", {Text = "This mode has bugs with any bloom modification; disabled that before use this mode."})
-			
-			CPanel:AddControl("CheckBox", { 
-				Label = "Lensflare Mode", 
-				Command = "pp_dlens_mode" 
-			})
-			
-			CPanel:AddControl("Label", {Text = "Change the bloom effect to a lensflare-like effect; recommended for use in dark or nighttime maps"})
-			
 		end
 	})
 	
 	local mat_Downsample = Material("pp/downsample")
 	local mat_Bloom = Material("dlenstexture/dlensmat")
 	local OldRT = render.GetRenderTarget()	
-	local RTBloom = GetRenderTarget("RtNewBloom", ScrW(), ScrH(), false)
+	local tex_Bloom0 = render.GetBloomTex0()
 	local w, h = ScrW(), ScrH()
 
-	local function DrawScene()
-		if not dl_lenson:GetBool() then return end
-		local mode = dl_mode:GetFloat()
-		if mode == 1 then
-			render.SetViewPort(0, 0, w, h)
-			render.SetRenderTarget(RTBloom)
-				render.Clear(0, 0, 0, 255, false)
-				render.ClearDepth()
-				local CamData = {
-				x = 0,
-				y = 0,
-				w = w,
-				h = h,
-				origin = LocalPlayer():EyePos(),
-				angles = LocalPlayer():EyeAngles() + Angle(0, 0, 180),
-				fov = dl_fov,
-				drawhud = false,
-				drawviewmodel = false,
-				dopostprocess = false,
-				}
-				render.RenderView(CamData)
-			render.SetRenderTarget(OldRT)
-			render.SetViewPort(0, 0, w, h)
-		end
-	end
-		
-	local function DrawLens()
-		if not dl_lenson:GetBool() then return end
+	hook.Add("RenderScreenspaceEffects", "DrawDirtLens", function()
+		if not dl_enable:GetBool() then return end
 		if not render.SupportsPixelShaders_2_0() then return end
         
-		local intensity = dl_intensity:GetFloat()
-		local blur = dl_blur:GetFloat()
-		local mul = dl_mul:GetFloat()
+		local blur = dl_blurrange:GetFloat()
+		local passes = dl_blurpasses:GetFloat()
+		local multiply = dl_multiply:GetFloat()
 		local darken = dl_darken:GetFloat()
-		local mode = dl_mode:GetFloat()
 		local lens_texture = dl_texture:GetString()
 		mat_Downsample:SetFloat("$darken", darken)
-		mat_Downsample:SetFloat("$multiply", mul)
-				
-		if mode == 0 then
-			mat_Downsample:SetTexture("$fbtexture", render.GetScreenEffectTexture())
-		elseif mode == 1 then
-			mat_Downsample:SetTexture("$fbtexture", RTBloom)
-		end
-		
-		render.SetViewPort(0, 0, w, h)
-		render.SetRenderTarget(RTBloom)
+		mat_Downsample:SetFloat("$multiply", multiply)	
+		mat_Downsample:SetTexture("$fbtexture", render.GetScreenEffectTexture())
+
+        	render.SetViewPort(0, 0, w, h)
+		render.SetRenderTarget(tex_Bloom0)
 			render.SetBlend(1)
 			render.SetMaterial(mat_Downsample)
 			render.DrawScreenQuad()
-			render.BlurRenderTarget(RTBloom, blur, blur, intensity)
+			render.BlurRenderTarget(tex_Bloom0, blur, blur, passes)
 			render.ClearDepth()
 		render.SetRenderTarget(OldRT)
-		render.SetViewPort(0, 0, w, h)
 		
 		mat_Bloom:SetFloat("$levelr", 1)
 		mat_Bloom:SetFloat("$levelg", 1)
 		mat_Bloom:SetFloat("$levelb", 1)
 		mat_Bloom:SetFloat("$colormul", 16)
-		mat_Bloom:SetTexture("$basetexture", RTBloom)
-
-		if mat_Bloom:GetTexture("$basetexture") ~= lens_texture then
-			mat_Bloom:SetTexture("$basetexture", lens_texture)
-			mat_Bloom:SetTexture("$dudvmap", lens_texture)
-			mat_Bloom:SetTexture("$normalmap", lens_texture)
-		end
-		mat_Bloom:SetTexture("$refracttinttexture", RTBloom)
+		
+		mat_Bloom:SetTexture("$basetexture", lens_texture)
+		mat_Bloom:SetTexture("$dudvmap", lens_texture)
+		mat_Bloom:SetTexture("$normalmap", lens_texture)
+		mat_Bloom:SetTexture("$refracttinttexture", tex_Bloom0)
 
 		render.SetMaterial(mat_Bloom)
 		render.DrawScreenQuad()
-	end
-    
-	hook.Add("RenderScene", "RenderDirtyLensBloom", DrawScene)
-	hook.Add("RenderScreenspaceEffects", "DrawDirtyLens", DrawLens)
-
+	end)
 end
