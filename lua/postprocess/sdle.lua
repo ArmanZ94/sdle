3d97142f9c817ef05c9516a5f53d41c2e9e6ff6c
Arifuwu <103443173+ArmanZ94@users.noreply.github.com>
2024-03-01 14:52:34 +0700

    Add Sun multiply - sdle.lua

--- lua/postprocess/sdle.lua
+++ lua/postprocess/sdle.lua
@@ -5,6 +5,9 @@ if CLIENT then
 	local dl_multiply = CreateClientConVar("pp_dlens_multiply", "1", true, false)
 	local dl_darken = CreateClientConVar("pp_dlens_darken", "0.30", true, false)
 	local dl_texture = CreateClientConVar("pp_dlens_texture", "dlenstexture/dlensdefault", true, false)
+	local dl_sunenable = CreateClientConVar("pp_dlens_sun", "1", true, false)
+	local dl_sunmultiply = CreateClientConVar("pp_dlens_sunmultiply", "2", true, false)
+	local dl_suninterpolation = CreateClientConVar("pp_dlens_suninterpolation", "6", true, false)
 
 	list.Set("PostProcess", "Dirt Lens", {
 		icon = "gui/postprocess/sdle.jpg",
@@ -22,9 +25,12 @@ if CLIENT then
 			params.Options["#preset.default"] = {
 				pp_dlens_blurrange = "15",
 				pp_dlens_blurpasses = "30",
-				pp_dlens_multiply = "1.30",
+				pp_dlens_multiply = "1",
 				pp_dlens_darken = "0.30",
-				pp_dlens_texture = "dlenstexture/dlensdefault"
+				pp_dlens_texture = "dlenstexture/dlensdefault",
+				pp_dlens_sun = "1",
+				pp_dlens_sunmultiply = "2",
+				pp_dlens_suninterpolation = "6"
 			}				
 				
 			params.CVars = table.GetKeys(params.Options["#preset.default"])
@@ -63,7 +69,7 @@ if CLIENT then
 				Command = "pp_dlens_blurrange",
 				Type = "Integer",
 				Min = "0",
-				Max = "50"
+				Max = "30"
 			})
 
 			CPanel:AddControl("Slider", {
@@ -79,7 +85,7 @@ if CLIENT then
 				Command = "pp_dlens_multiply",
 				Type = "Float",
 				Min = "0",
-				Max = "10"
+				Max = "5"
 			})
 	
 			CPanel:AddControl("Slider", {
@@ -89,6 +95,27 @@ if CLIENT then
 				Min = "0",
 				Max = "1"
 			})
+			
+			CPanel:AddControl("CheckBox", { 
+				Label = "Enable Sun Dirt Lens", 
+				Command = "pp_dlens_sun" 
+			})
+			
+			CPanel:AddControl("Slider", {
+				Label = "Sun Multiply",
+				Command = "pp_dlens_sunmultiply",
+				Type = "Float",
+				Min = "0",
+				Max = "5"
+			})
+			
+			CPanel:AddControl("Slider", {
+				Label = "Sun Interpolation Speed",
+				Command = "pp_dlens_suninterpolation",
+				Type = "Integer",
+				Min = "1",
+				Max = "20"
+			})
 		end
 	})
 	
@@ -97,6 +124,8 @@ if CLIENT then
 	local OldRT = render.GetRenderTarget()	
 	local tex_Bloom0 = render.GetBloomTex0()
 	local w, h = ScrW(), ScrH()
+	local targetMultiply = 1
+	local multiply = 1
 
 	hook.Add("RenderScreenspaceEffects", "DrawDirtLens", function()
 		if not dl_enable:GetBool() then return end
@@ -104,14 +133,32 @@ if CLIENT then
         
 		local blur = dl_blurrange:GetFloat()
 		local passes = dl_blurpasses:GetFloat()
-		local multiply = dl_multiply:GetFloat()
 		local darken = dl_darken:GetFloat()
 		local lens_texture = dl_texture:GetString()
-		mat_Downsample:SetFloat("$darken", darken)
+		local SunMultiply = dl_sunmultiply:GetFloat()
+		local SunInterpolation = dl_suninterpolation:GetInt()
+
+		if dl_sunenable:GetBool() then
+			local sun = util.GetSunInfo()
+			if sun then
+				if ( sun.obstruction ~= 0 ) then
+					targetMultiply = dl_multiply:GetFloat() + SunMultiply
+				else
+					targetMultiply = dl_multiply:GetFloat()
+				end	
+			else
+				targetMultiply = dl_multiply:GetFloat()
+			end
+			multiply = Lerp(FrameTime() * SunInterpolation, multiply, targetMultiply)
+		else
+			multiply = dl_multiply:GetFloat()
+		end
+		
 		mat_Downsample:SetFloat("$multiply", multiply)	
+		mat_Downsample:SetFloat("$darken", darken)
 		mat_Downsample:SetTexture("$fbtexture", render.GetScreenEffectTexture())
 
-        	render.SetViewPort(0, 0, w, h)
+     		render.SetViewPort(0, 0, w, h)
 		render.SetRenderTarget(tex_Bloom0)
 			render.SetBlend(1)
 			render.SetMaterial(mat_Downsample)
